9f48a40a17ae9c4d6ee316ce9895940d1fdf6433
kmartinet <kmartinet@outlook.com>
2025-11-05 16:13:57 -0700

    Added Eleutherodactylus taxon-specific CSV

--- Caribbean/Caribbean_Models.R
+++ Caribbean/Caribbean_Models.R
@@ -27,37 +27,69 @@ final_df <- merge(dem_stats, isolation, by = "name", all.x = TRUE, all.y = TRUE)
 frog_dat <- read.csv("Caribbean/Data/Eleutherodactylus_Curated.csv")
 richness <- as.data.frame(table(frog_dat$First))
 colnames(richness) <- c("name", "richness")
-# Some of the island names don't match the rest of the data
-richness[,1] <- as.character(richness[,1])
-richness[9,1] <- "Seymour Norte"
-richness[15,1] <- "Santa Fé"
-richness[3,1] <- "Española"
-richness[11,1] <- "Pinzón"
+richness$name <- as.character(richness$name)
+richness$richness <- as.numeric(richness$richness)
+# Some names don't match
+richness[1,1] <- "Andros"
+richness[7,1] <- "Beef"
+richness[8,1] <- "Bell Island"
+richness[10,1] <- "Cat"
+richness[11,1] <- "Cayman Brac Island"
+richness[13,1] <- "de Culebra"
+richness[16,1] <- "Eleuthera"
+richness[17,1] <- "Grand Bahama"
+richness[22,1] <- "Guana"
+richness[25,1] <- "Juventud"
+richness[26,1] <- "Mona"
+richness[30,1] <- "Long"
+richness[36,1] <- "North Caicos"
+richness[40,1] <- "St. Croix"
+richness[41,1] <- "St. John"
+richness[43,1] <- "Saint Martin/Sint Maarten"
+richness[45,1] <- "Shroud Cay"
+richness[46,1] <- "South Bimini Cay"
+richness[51,1] <- "Tortue"
+richness[52,1] <- "Viegues"
+richness[54,1] <- "West End Cay"
 
 # Merge richness to final dataframe
 final_df <- merge(final_df, richness, by = "name", all.x = TRUE, all.y = TRUE)
 
 # Add areas to final dataframe
-areas_df <- read.csv("Galapagos/Data/galapagos_areas.csv")
+areas_df <- read.csv("Caribbean/Data/carib_areas.csv")
 final_df <- merge(final_df, areas_df, by = "name", all.x = TRUE)
 
 # Add CSI to final dataframe
-csi <- read.csv("Galapagos/Data/csi.csv")
+csi <- read.csv("Caribbean/Data/csi.csv")
 colnames(csi) <- c("name", "mean_csi", "sd_csi")
 final_df <- merge(final_df, csi, by = "name", all.x = TRUE)
 
 # Replace NAs with 0
 final_df[is.na(final_df)] <- 0
 
-# Run GLMM?
-# model1 <- glmer(richness ~ TRI + mean_elev + median_elev + min_elev + max_elev + Nearest_Dist + nearest_occ, 
-#                data = final_df, family = poisson)
+# Remove rows where area = 0 (that means no island info)
+final_df <- final_df[-which(final_df$area == 0),]
 
-# The finch SAR doesn't have a breakpoint, so actually it should be a glm?
-model1 <- glm(richness ~ TRI + mean_elev + median_elev + min_elev + max_elev + Nearest_Dist + nearest_occ + mean_csi + sd_csi,
-              family = poisson, data = final_df)
-model2 <- glm(richness ~ TRI + mean_elev + median_elev + min_elev + max_elev + Nearest_Dist + nearest_occ + mean_csi + sd_csi + area,
-              family = poisson, data = final_df)
+# Add whether the island is before/after the breakpoint
+# The breakpoint here is 21.114, so any log(area) above that is after the BP
+bp <- rep(0, 408)
+for(i in c(1:length(final_df$area))){
+  if(log(final_df[i,10]) > 21.114){
+    bp[i] <- 1
+  }
+}
+
+final_df <- cbind(final_df, bp)
+
+write.csv(final_df, "Caribbean/carib_eleutherodactylus_final_df.csv", row.names = FALSE)
+
+# Run GLMM?
+# The Eleutherodactylus SAR has a breakpoint and a positive second slope
+# nearest_occ might actually be fine to include here
+model1 <- glmer(richness ~ TRI + mean_elev + median_elev + min_elev + max_elev + Nearest_Dist + mean_csi + sd_csi + (1 | bp), 
+                data = final_scale, family = poisson)
+model2 <- glmer(richness ~ TRI + mean_elev + median_elev + min_elev + max_elev + Nearest_Dist + mean_csi + sd_csi + area + (1 | bp), 
+                data = final_df, family = poisson)
 
 ##### Galapagos Scalesia #####
 
